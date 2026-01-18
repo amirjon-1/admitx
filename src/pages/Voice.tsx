d839734dae9d2c18d3c30e43234ce1613dcacbfd
Adit <adit150109@gmail.com>
2026-01-18 08:16:55 -0500

    fixed

--- src/pages/Voice.tsx
+++ src/pages/Voice.tsx
@@ -23,7 +23,7 @@ export function Voice() {
         className="rounded-lg bg-blue-600 text-white px-4 py-2"
         onClick={handleConnect}
       >
-        Connect
+        Connect.
       </button>
     </div>
   );
